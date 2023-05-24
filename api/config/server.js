cd4fdfaf24687760d86abade3e32ea21be175c98
PELUMIJOY <faslusky@gmail.com>
2023-05-24 05:25:55 +0100

    api config

--- api/config/server.js
+++ api/config/server.js
@@ -7,4 +7,14 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  contentSecurity: {
+    // ...
+    directives: {
+      // ...
+      connectSrc: ["'self'", "http://localhost:1337", "https:"],
+      // ...
+    },
+    // ...
+  },
+  
 });
