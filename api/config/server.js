a444c4d0da5693f239552532d5c9c0a26e09a615
PELUMIJOY <faslusky@gmail.com>
2023-05-25 10:06:44 +0100

    api

--- api/config/server.js
+++ api/config/server.js
@@ -1,20 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  // host: env('HOST'),
   port: env.int('PORT', 1337),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  contentSecurity: {
-    // ...
-    directives: {
-      // ...
-      connectSrc: ["'self'", "http://localhost:1337", "https:"],
-      // ...
-    },
-    // ...
-  },
-  
+
 });
