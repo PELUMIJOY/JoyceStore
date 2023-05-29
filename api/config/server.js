8f7e9a7a4cf4c333b7b09a5c2a2b09f95ef8a071
PELUMIJOY <faslusky@gmail.com>
2023-05-29 15:42:50 +0100

    server/connectSrc

--- api/config/server.js
+++ api/config/server.js
@@ -1,25 +1,47 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST'),
+//   port: env.int('PORT', 1337),
+
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   // url:"www.mystore.com",
+  
+
+
+  
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+//   contentSecurity: {
+//     // ...
+//     directives: {
+//       // ...
+//       connectsrc: ["'self'", "www.mysample.dev", "https:"],
+//       // ...
+//     },
+//     // ...
+//   },
+  
+// });
+
 module.exports = ({ env }) => ({
   host: env('HOST'),
   port: env.int('PORT', 1337),
 
+  // Add the `settings` property to set the Content-Security-Policy header
+  settings: {
+    security: {
+      // Modify the Content-Security-Policy directive
+      contentSecurityPolicy: {
+        directives: {
+          connectSrc: ["'self'", env('RENDER_HOST')],
+        },
+      },
+    },
+  },
+
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // url:"www.mystore.com",
-
-
-  
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  contentSecurity: {
-    // ...
-    directives: {
-      // ...
-      connectSrc: ["'self'", "www.mysample.dev", "https:"],
-      // ...
-    },
-    // ...
-  },
-  
 });
